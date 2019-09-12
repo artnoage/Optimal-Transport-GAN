19f1c218554f80107f4d86e7b6a18ec26bc1bcf7
artnoage <artnoage@gmail.com>
2019-09-12 14:11:01 +0200

    art

--- src/models/perfect_ass.py
+++ src/models/perfect_ass.py
@@ -14,33 +14,43 @@ class Vaios_model:
 
 
     def define_graph(self):
-        self.z = self.latent.tensor()
-        self.real_batch_ph = tf.placeholder(shape=(None, self.dataset.get_total_shape()), dtype=tf.float32,
-                                            name="real_batch_ph")
-        self.dataset_size = self.dataset.dataset_size
+        self.generate_latent_batch = self.latent.tensor()
         self.latent_batch_ph = tf.placeholder(shape=(None, self.latent.shape), dtype=tf.float32)
-        self.fake_samples = self.gen_network.tensor(self.z, self.dataset.get_total_shape())
-        self.crit_fake = self.crit_network.tensor(self.fake_samples,xdim=512)
+        self.generate_fake_samples = self.gen_network.tensor(self.generate_latent_batch, self.dataset.get_total_shape())
+        self.feeded_generated_batch = self.gen_network.tensor(self.latent_batch_ph, self.dataset.get_total_shape())
+
+        self.real_batch_ph = tf.placeholder(shape=(None, self.dataset.get_total_shape()), dtype=tf.float32, name="real_batch_ph")
+        self.real_idx_ph = tf.placeholder(tf.int32, shape=(None,))
+
+
+        self.crit_fake = self.crit_network.tensor(self.generate_fake_samples, xdim=512)
         self.crit_real_ph = tf.placeholder(tf.float32,name="crit_real")
         self.best_idx_ph = tf.placeholder(tf.int32, shape=(None,), name="best_idx")
-        self.index_total_size_ph = tf.placeholder(tf.int32, shape=(), name="index_total_size")
+
         self.assign_samples_ph = tf.placeholder(tf.float32, shape=(None, self.dataset.get_total_shape()))
         self.n_assign_ph = tf.placeholder(tf.float32, shape=(None,))
-        self.crit_train_op = self.crit_network.train_op(self.assignment_critic_cost())
+
 
         self.find_couples=self.find_couples_unlimited_ssim()
-
-        self.real_idx_ph = tf.placeholder(tf.int32, shape=(None,))
-        self.fake_batch = self.gen_network.tensor(self.latent_batch_ph, self.dataset.get_total_shape())
         self.gen_cost = self.assignment_generator_cost_ssim()
-        self.gen_train_op = self.gen_network.train_op(self.gen_cost)
 
+        self.gen_train_op = self.gen_network.train_op(self.gen_cost)
+        self.crit_train_op = self.crit_network.train_op(self.assignment_critic_cost())
 
     def find_assignments_critic(self,session, assign_loops=100):
-        assign_arr = np.zeros(shape=(self.dataset_size,))
+        ratio = int(self.dataset.dataset_size / self.dataset.batch_size)
+        assign_arr = np.zeros(shape=(self.dataset.dataset_size,))
         latent_sample_list, real_idx_list = [], []
         for assign_loop in range(assign_loops):
-            latent_points, current_best = session.run(self.find_couples)
+            latent_points=session.run(self.generate_latent_batch)
+            for ith in range(ratio):
+                index=np.arange(ith*self.dataset.batch_size,(ith+1)*self.dataset.batch_size)
+                if ith==0:
+                    all_index=index
+                if ith!=0:
+                    all_index=np.concatenate([current_best,index],axis=0)
+                best = session.run(self.find_couples,feed_dict={self.real_batch_ph:self.dataset.data[all_index],self.latent_batch_ph:latent_points})
+                current_best = all_index[best]
             assign_c = np.reshape(current_best, newshape=[-1, 1])
             latent_sample_list.append(latent_points)
             real_idx_list.append(assign_c)
@@ -49,67 +59,33 @@ class Vaios_model:
         return assign_arr,latent_sample_list,real_idx_list
 
     def find_couples_unlimited_ssim(self):
-        ratio=int(self.dataset_size/ self.dataset.batch_size)
-        latent_points= self.z
-        fake_samples= self.fake_samples
+        fake_samples= self.feeded_generated_batch
         latent_batch_size = self.latent.batch_size
         single_image_shape = self.dataset.shape
         fake_samples_shape = (-1,) + single_image_shape
         real_matrix_shape = (-1,) + single_image_shape
-        index = np.arange(0,  self.dataset.batch_size)
-        real_points = self.dataset.data[index]
-        new1 = tf.keras.backend.repeat_elements(
-            tf.reshape(real_points,
-                       real_matrix_shape
-                       ),
-            latent_batch_size,
-            axis=0
-        )
-        new2 = tf.tile(tf.reshape(fake_samples, fake_samples_shape), [index.size, 1, 1, 1])
+        real_points = self.real_batch_ph
+        new1 = tf.keras.backend.repeat_elements(tf.reshape(real_points, real_matrix_shape), latent_batch_size, axis=0)
+        new2 = tf.tile(tf.reshape(fake_samples, fake_samples_shape), [tf.shape(real_points)[0], 1, 1, 1])
         new1 = new1 + 1
         new2 = new2 + 1
-        dist = 1 - tf.image.ssim(
+        dist = 1 - tf.image.psnr(
             new1,
             new2,
             2
         )
 
-        dist = tf.transpose(self.crit_network.tensor(real_points)) + tf.transpose(tf.reshape(dist, (index.size, tf.shape(fake_samples)[0])))
+        dist = tf.transpose(self.crit_network.tensor(real_points)) + tf.transpose(
+            tf.reshape(dist, (tf.shape(real_points)[0], tf.shape(fake_samples)[0])))
         couples  = tf.argmin(dist, axis=1, output_type=tf.int32)
-        minimizers=couples
-        for ith_batch in range(1,ratio):
-            pro_reals = tf.gather(real_points, minimizers)
-            index = np.arange(ith_batch * self.dataset.batch_size, (ith_batch + 1) * self.dataset.batch_size)
-            indexed_data = self.dataset.data[index]
-            all_index=tf.concat((couples,index),axis=0)
-            real_points =tf.concat((pro_reals,indexed_data),axis=0)
-            new1 = tf.keras.backend.repeat_elements(
-                tf.reshape(real_points,
-                           real_matrix_shape
-                           ),
-                latent_batch_size,
-                axis=0
-            )
-            new2 = tf.tile(tf.reshape(fake_samples,fake_samples_shape), [tf.shape(real_points)[0], 1, 1, 1])
-            new1 = new1+1
-            new2 = new2+1
-            dist = 1-tf.image.ssim(
-                new1,
-                new2,
-                2
-            )
-            dist = tf.transpose(self.crit_network.tensor(real_points))\
-            + tf.transpose(tf.reshape(dist, (tf.shape(real_points)[0], tf.shape(fake_samples)[0])))
-            minimizers=tf.argmin(dist, axis=1, output_type=tf.int32)
-            couples=tf.gather(all_index,minimizers)
-        return latent_points, couples
+        return couples
 
     def find_couples_unlimited_square(self):
         ratio=int(self.dataset_size/ self.dataset.batch_size)
-        latent_points= self.z
+        latent_points= self.generate_latent_batch
         index = np.arange(0,  self.dataset.batch_size)
         real_points = tf.gather(self.real_matrix, index, axis=0)
-        z = tf.expand_dims(self.fake_samples, axis=1) - real_points
+        z = tf.expand_dims(self.generate_fake_samples, axis=1) - real_points
         norm_mat = 0.1 * tf.square(tf.norm(z, axis=2))
         dist=tf.transpose(self.crit_network.tensor(real_points)) + norm_mat
         couples = tf.argmin(dist, axis=1, output_type=tf.int32)
@@ -117,7 +93,7 @@ class Vaios_model:
             index = np.arange(ith_batch * self.dataset.batch_size, (ith_batch + 1) * self.dataset.batch_size)
             all_index = tf.concat((couples, index),axis=0)
             real_points = tf.gather(self.real_matrix, all_index, axis=0)
-            z = tf.expand_dims(self.fake_samples, axis=1) - real_points
+            z = tf.expand_dims(self.generate_fake_samples, axis=1) - real_points
             norm_mat = 0.1 * tf.square(tf.norm(z, axis=2))
             dist = tf.transpose(self.crit_network.tensor(real_points)) + norm_mat
             minimizers=tf.argmin(dist, axis=1, output_type=tf.int32)
@@ -128,19 +104,20 @@ class Vaios_model:
 
 
     def generate_samples(self,session,latent_sample):
-        return session.run(self.fake_batch, feed_dict={self.latent_batch_ph: latent_sample[0:5000]})
+        return session.run(self.feeded_generated_batch, feed_dict={self.latent_batch_ph: latent_sample[0:5000]})
 
     def get_fake_tensor(self):
-        return self.fake_batch
+        return self.feeded_generated_batch
 
 
 
     def assignment_critic_cost(self):
+        weak_idx= np.random.choice(range(self.dataset.dataset_size), self.dataset.batch_size, replace=False)
         crit_assign = self.crit_network.tensor(self.assign_samples_ph,xdim=512)
         crit_assign_weighted = tf.multiply(self.n_assign_ph, tf.squeeze(crit_assign))
         crit_cost = -(
                 (tf.reduce_sum(crit_assign_weighted) / tf.reduce_sum(self.n_assign_ph))
-                - tf.reduce_mean(self.crit_network.tensor(self.dataset.data[0:self.dataset.dataset_size])))
+                - tf.reduce_mean(self.crit_network.tensor(self.dataset.data[weak_idx])))
         return crit_cost
 
 
@@ -149,16 +126,16 @@ class Vaios_model:
         batch_image_shape = (-1,) + single_image_shape
         self.real_batch = self.real_batch_ph
         return tf.reduce_mean(
-            1-tf.image.ssim(tf.reshape(self.real_batch,batch_image_shape)+1 ,
-                          tf.reshape(self.fake_batch,batch_image_shape)+1
-                          ,2
+            1-tf.image.ssim(tf.reshape(self.real_batch,batch_image_shape) + 1,
+                            tf.reshape(self.feeded_generated_batch, batch_image_shape) + 1
+                            , 2
 
 
-            )
+                            )
         )
     def assignment_generator_cost_square(self):
         self.real_batch = self.real_batch_ph
-        subed_batches = self.fake_batch - self.real_batch
+        subed_batches = self.feeded_generated_batch - self.real_batch
         gen_cost = tf.reduce_mean(tf.square(tf.norm(subed_batches, axis=0)))
         return gen_cost
 
--- src/perfect_train.py
+++ src/perfect_train.py
@@ -62,10 +62,10 @@ class AssignmentTraining_new_approach:
                             crit_bar.set_description(
                                 "step 1: # zeros " + str(self.n_zeros) + " Variance" + str(np.var(assign_arr)),
                                 refresh=False)
-                            latent_sample_big = latent_sample_big + latent_sample
-                            real_idx_big = real_idx_big + real_idx
-                latent_sample_big = np.vstack(tuple(latent_sample_big))
-                real_idx_big = np.vstack(tuple(real_idx_big)).flatten()
+                            latent_sample
+                            real_idx
+                latent_sample_big = np.vstack(tuple(latent_sample))
+                real_idx_big = np.vstack(tuple(real_idx)).flatten()
                 self.model.train_generator(session, real_idx_big,latent_sample_big,offset=100)
                 session.run(self.increase_global_step)
                 fakes = session.run(self.model.get_fake_tensor(), {self.model.latent_batch_ph: latent_sample_big[:18]})
@@ -80,11 +80,11 @@ class AssignmentTraining_new_approach:
 
         # accumulate some real and fake samples
         if max_loop-1 == main_loop:
-            fake_points = self.session.run(self.model.fake_samples)
+            fake_points = self.session.run(self.model.generate_fake_samples)
             dataset_dim = self.dataset.get_total_shape()
             n_fake_to_save = 100000
             while(fake_points.shape[0]<n_fake_to_save):
-                fake_points_new = self.session.run(self.model.fake_samples)
+                fake_points_new = self.session.run(self.model.generate_fake_samples)
                 fake_points = np.vstack((fake_points, fake_points_new))
             dump_path =  "logs" + os.sep + self.experiment_name+os.sep
             np.save(dump_path + "fakes", fake_points)
@@ -92,15 +92,15 @@ class AssignmentTraining_new_approach:
 
 def main():
     Settings.setup_enviroment(gpu=0)
-    assignment_training = AssignmentTraining_new_approach(dataset=Mnist32(batch_size=100,dataset_size=200),
-                                                          latent=VaiosLatent2(shape=20, batch_size=200),
+    assignment_training = AssignmentTraining_new_approach(dataset=Mnist32(batch_size=600,dataset_size=3000),
+                                                          latent=VaiosLatent2(shape=20, batch_size=50),
                                                           cost_type=CostType.ASSIGNMENT,
                                                           critic_network=ConvNew32(name="critic", learn_rate=1e-5,
                                                                                    layer_dim=512),
                                                           generator_network=DeconvNew32(name="generator",
                                                                                         learn_rate=1e-4, layer_dim=512)
                                                           )
-    assignment_training.train(n_main_loops=750, n_critic_loops=10, n_assign_loops=2)
+    assignment_training.train(n_main_loops=750, n_critic_loops=10, n_assign_loops=10)
 
 if __name__ == "__main__":
     main()
