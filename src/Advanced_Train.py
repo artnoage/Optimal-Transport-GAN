55e00f111d42b91fdc97631f1cfa065e8446d9e1
artnoage <artnoage@gmail.com>
2019-09-28 17:49:08 +0200

    sa

--- src/Advanced_Train.py
+++ src/Advanced_Train.py
@@ -49,9 +49,13 @@ class AssignmentTraining:
             global_step = session.run(self.global_step)
             self.n_zeros = self.latent.batch_size
             for main_loop in tqdm.tqdm(range(global_step, n_main_loops, 1)):
-                local_dataset_s = int(min(5*(main_loop)+200, self.dataset.dataset_size))
-                assignment_loops = int(20*((main_loop/n_main_loops)**2) * local_dataset_s / self.latent.batch_size) + 20
-                with tqdm.tqdm(range(n_critic_loops)) as crit_bar:
+                local_dataset_s = int(min((self.dataset.dataset_size)*(2*main_loop/n_main_loops)+200, self.dataset.dataset_size))
+                assignment_loops = int(10*local_dataset_s / self.latent.batch_size) + 10
+                if main_loop<200:
+                    croop=12
+                else:
+                    croop=2
+                with tqdm.tqdm(range(croop)) as crit_bar:
                     for crit_loop in crit_bar:
                         assign_arr, latent_sample,real_idx = self.model.find_assignments_critic(session, assign_loops= assignment_loops, local_dataset_size=local_dataset_s)
                         self.model.train_critic(session, assign_arr, local_dataset_s)
@@ -62,7 +66,7 @@ class AssignmentTraining:
 
                 latent_sample = np.vstack(tuple(latent_sample))
                 real_idx = np.vstack(tuple(real_idx)).flatten()
-                self.model.train_generator(session, real_idx,latent_sample,offset=5)
+                self.model.train_generator(session, real_idx,latent_sample,offset=4)
                 session.run(self.increase_global_step)
 
                 # It makes images for Tensorboard
@@ -93,11 +97,11 @@ class AssignmentTraining:
 def main():
     Settings.setup_enviroment(gpu=0)
     assignment_training = AssignmentTraining(dataset=Fashion32(batch_size=5000, dataset_size=5000),
-                                             latent=Assignment_latent(shape=250, batch_size=400),
+                                             latent=Assignment_latent(shape=300, batch_size=400),
                                              critic_network=DenseCritic(name="critic", learn_rate=5e-5,layer_dim=512,xdim=32*32*1),
                                              generator_network=DeconvNew32(name="generator",learn_rate=1e-4, layer_dim=512,channels=1),
                                              cost="square")
-    assignment_training.train(n_main_loops=1000, n_critic_loops=2)
+    assignment_training.train(n_main_loops=1200, n_critic_loops=3)
 
 if __name__ == "__main__":
     main()
