f6a9bac77900e86494252ce5a99e5e52b888e547
artnoage <artnoage@gmail.com>
2019-10-04 14:02:38 +0200

    art

--- src/models/Assignment_model.py
+++ src/models/Assignment_model.py
@@ -188,4 +188,4 @@ class Assignment_model:
                                 self.latent_batch_ph: latent_sample[c_idx:c_idx + offset]})
             step_2_dict.update({self.gen_network.get_training_placeholder(): True})
             _, A = session.run([self.gen_train_op, self.gen_cost], step_2_dict)
-            print(A)
+            print("The trasportation distance for this batch is", A)
