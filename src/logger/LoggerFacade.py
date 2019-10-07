258688c3a77dfffa8e18b73677ccf36dbc4af5f3
HeavyPanda <Jantinapp@hotmail.com>
2019-10-07 14:18:25 +0200

    Removed methods that are not needed anymore in logging

--- src/logger/LoggerFacade.py
+++ src/logger/LoggerFacade.py
@@ -1,27 +1,26 @@
 import os
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 from logger.LoggerNew import LoggerNew
+from logger.Plotting import make_image_grid
 from logger.SummaryType import SummaryType
-from logger.Plotting import generate_density, plot_fake_and_real, make_image_grid, wasserstein_distance
-import matplotlib.pyplot as plt
 
 
 class LoggerFacade:
     """
     A Facade to give an easy interface to logging data to the tensorboard logs
     """
+
     def __init__(self, session, writer, dataset_shape):
         self.session = session
         self.writer = writer
         self.logger = LoggerNew(session, writer)
         self.dataset_shape = dataset_shape
 
-
-
-
-    def log_image_grid_fixed(self, first_rows, second_rows, n_iterations, name="real_and_fake_images",save_image=False,):
+    def log_image_grid_fixed(self, first_rows, second_rows, n_iterations, name="real_and_fake_images",
+                             save_image=False, ):
         """
         logs the given points as a grid to tensorboard
 
@@ -38,16 +37,8 @@ class LoggerFacade:
         combined_grid = np.expand_dims(combined_grid, axis=0)
         self.logger.write_to_log(combined_grid, name, n_iterations, SummaryType.IMAGE)
         if save_image:
-            self._to_image(combined_grid.squeeze(),n_iterations)
+            self._to_image(combined_grid.squeeze(), n_iterations)
 
-
-
-    def _to_image(self,matrix,n_iteration,name="fakes"):
+    def _to_image(self, matrix, n_iteration, name="fakes"):
         plt.imshow(matrix)
-        plt.savefig(self.logger.writer.get_logdir()+os.sep+name+"_"+str(n_iteration))
-
-
-
-
-
-
+        plt.savefig(self.logger.writer.get_logdir() + os.sep + name + "_" + str(n_iteration))
